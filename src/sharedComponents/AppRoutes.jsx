bef749d76cd36c956f557892ae38f7f355d4b83e
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2025-03-11 08:53:03 -0400

    complete form page

--- src/sharedComponents/AppRoutes.jsx
+++ src/sharedComponents/AppRoutes.jsx
@@ -3,12 +3,14 @@ import Dashboard from "../scenes/dashboard";
 import Team from "../scenes/team";
 import Contacts from "../scenes/contacts";
 import Invoices from "../scenes/invoices";
+import Form from "../scenes/form";
 
 const routes = [
     { path: "/", element: <Dashboard  /> },
     { path: "/team", element: <Team  /> },
     { path: "/contacts", element: <Contacts  /> },
-    { path: "/invoices", element: <Invoices  /> }
+    { path: "/invoices", element: <Invoices  /> },
+    { path: "/form", element: <Form  /> }
 ];
  
 const AppRoutes = () => {
