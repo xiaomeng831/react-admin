d9b092b2275861ca0ca432c1862972bd6f192beb
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2025-03-10 22:05:12 -0400

    complete 4 pages

--- src/sharedComponents/AppRoutes.jsx
+++ src/sharedComponents/AppRoutes.jsx
@@ -1,8 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import Dashboard from "../scenes/dashboard";
+import Team from "../scenes/team";
+import Contacts from "../scenes/contacts";
+import Invoices from "../scenes/invoices";
 
 const routes = [
-    { path: "/", element: <Dashboard  /> }
+    { path: "/", element: <Dashboard  /> },
+    { path: "/team", element: <Team  /> },
+    { path: "/contacts", element: <Contacts  /> },
+    { path: "/invoices", element: <Invoices  /> }
 ];
  
 const AppRoutes = () => {
