511a849e9115ea0984a0d799469f347bc18252aa
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2025-03-12 19:47:26 -0400

    finish Calendar

--- src/sharedComponents/AppRoutes.jsx
+++ src/sharedComponents/AppRoutes.jsx
@@ -4,13 +4,15 @@ import Team from "../scenes/team";
 import Contacts from "../scenes/contacts";
 import Invoices from "../scenes/invoices";
 import Form from "../scenes/form";
+import Calendar from "../scenes/calendar";
 
 const routes = [
     { path: "/", element: <Dashboard  /> },
     { path: "/team", element: <Team  /> },
     { path: "/contacts", element: <Contacts  /> },
     { path: "/invoices", element: <Invoices  /> },
-    { path: "/form", element: <Form  /> }
+    { path: "/form", element: <Form  /> },
+    { path: "/calendar", element: <Calendar  /> }
 ];
  
 const AppRoutes = () => {
