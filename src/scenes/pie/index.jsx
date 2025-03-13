f5a1da48a846da1e0840d8b0b995ae8b222b99cd
xiaomeng831 <78976407+xiaomeng831@users.noreply.github.com>
2025-03-13 10:18:46 -0400

    complete faq, barchart, piechart, linechart

---
+++ src/scenes/pie/index.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Header from "../../sharedComponents/Header";
+import PieChart from "../../sharedComponents/PieChart";
+
+const Pie = () => {
+  return (
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Pie;
--- src/sharedComponents/AppRoutes.jsx
+++ src/sharedComponents/AppRoutes.jsx
@@ -5,6 +5,10 @@ import Contacts from "../scenes/contacts";
 import Invoices from "../scenes/invoices";
 import Form from "../scenes/form";
 import Calendar from "../scenes/calendar";
+import Faq from "../scenes/faq";
+import Bar from "../scenes/bar";
+import Pie from "../scenes/pie";
+import Line from "../scenes/line";
 
 const routes = [
     { path: "/", element: <Dashboard  /> },
@@ -12,7 +16,11 @@ const routes = [
     { path: "/contacts", element: <Contacts  /> },
     { path: "/invoices", element: <Invoices  /> },
     { path: "/form", element: <Form  /> },
-    { path: "/calendar", element: <Calendar  /> }
+    { path: "/calendar", element: <Calendar  /> },
+    { path: "/faq", element: <Faq  /> },
+    { path: "/bar", element: <Bar  /> },
+    { path: "/pie", element: <Pie  /> },
+    { path: "/line", element: <Line  /> }
 ];
  
 const AppRoutes = () => {
